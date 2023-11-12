dc2fc61473386f4c2b75be5a5b09e331094af6fb
Pushkar S <spushkar0071234@gmail.com>
2023-11-12 13:34:39 +0530

    Wrote the header file

---
+++ sorting.h
@@ -0,0 +1,7 @@
+void insertion_sort(int* arr, int n);
+void bubble_sort(int* arr, int n);
+void selection_sort(int* arr, int n);
+void heap_sort(int* arr, int n);
+void merge_sort(int* arr, int n);
+void quick_sort(int* arr, int n);
+void print_histogram(int* arr, int n);
