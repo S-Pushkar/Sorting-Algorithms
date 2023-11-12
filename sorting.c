aea6a7bf0b221042c56edc0f6707cec315940323
rahul <rahul.baradol.14@gmail.com>
2023-11-12 17:54:34 +0530

    bubble sort

--- sorting.c
+++ sorting.c
@@ -54,7 +54,27 @@ void insertion_sort(int* arr, int n) {
 }
 
 void bubble_sort(int* arr, int n) {
-
+    print_histogram(arr, n, 0);
+    int i, j;
+    int itr = 1;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+                swapped = 1;
+            }
+        }
+ 
+        // If no two elements were swapped by inner loop,
+        // then break
+        print_histogram(arr, n, itr++);
+        if (swapped == 0)
+            break;
+    }
 }
 
 void selection_sort(int* arr, int n) {
