8fab15f3eba7531c3515a43de222658df2afdf3b
RISHITH125 <rishithp125@gmail.com>
2023-11-13 06:11:02 +0530

    Heap sort algorithm implimented

--- sorting.c
+++ sorting.c
@@ -108,61 +108,40 @@ void selection_sort(int* arr, int n) {
         print_histogram(arr, n, itr++);
     }
 }
+void heapify(int* arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-void heap_sort(int* arr, int n) {
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 = r - m; 
-  
-    // Create temp arrays 
-    int L[n1], R[n2]; 
-  
-    // Copy data to temp arrays 
-    // L[] and R[] 
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1 + j]; 
-  
-    // Merge the temp arrays back 
-    // into arr[l..r] 
-    // Initial index of first subarray 
-    i = 0; 
-  
-    // Initial index of second subarray 
-    j = 0; 
-  
-    // Initial index of merged subarray 
-    k = l; 
-    while (i < n1 && j < n2) { 
-        if (L[i] <= R[j]) { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    // Copy the remaining elements 
-    // of L[], if there are any 
-    while (i < n1) { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    // Copy the remaining elements of 
-    // R[], if there are any 
-    while (j < n2) { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
 }
+void heap_sort(int* arr, int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
 
+    int itr = 1;
+
+    // One by one extract an element from the heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(&arr[0], &arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+
+        print_histogram(arr, n, itr++);
+    }
+}
 void merge(int *arr, int l, int m, int r, int n, int *iter) 
 { 
     int i, j, k; 
